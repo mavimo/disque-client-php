f28b84045bca28f9708ab1893c430687e91071b1
Marco Vito Moscaritolo <mavimo@gmail.com>
2015-05-02 22:09:58 +0200

    Added some test on adding element to queue.

--- tests/Client/ClientTest.php
+++ tests/Client/ClientTest.php
@@ -10,22 +10,25 @@ namespace Mavimo\Tests\Disque\Client;
 
 use Mavimo\Disque\Client\Client;
 use Mavimo\Disque\Client\SocketInterface;
+use Mavimo\Disque\Job\Job;
+use Mavimo\Disque\Queue\Queue;
+
+use Prophecy\Argument;
 
 class ClientTest extends \PHPUnit_Framework_TestCase
 {
-    protected $client;
-
-    protected function setUp()
+    public function testJobIdUpdateAfterPush()
     {
+        $jobIds = 'DI0f0c644fd3ccb51c2cedbd47fcb6f312646c993c05a0SQ';
         $socket = $this->prophesize('Mavimo\Disque\Client\SocketInterface');
+        $socket->sendCommand(Argument::any())->willReturn($jobIds);
 
-        $this->client = new Client($socket->reveal());
-    }
+        $client = new Client($socket->reveal());
+        $job = new Job('test');
+        $queue = new Queue('test');
 
-    public function testSkip()
-    {
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $client->push($queue, $job);
+
+        $this->assertEquals($jobIds, $job->getIds());
     }
 }
